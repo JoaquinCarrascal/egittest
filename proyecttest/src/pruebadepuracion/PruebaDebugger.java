a91be6a7ddff85724b71f393f3ec008fc1013040
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2023-11-25 13:03:06 +0100

    commit del debug de egit

---
+++ proyecttest/src/pruebadepuracion/PruebaDebugger.java
@@ -0,0 +1,12 @@
+package pruebadepuracion;
+
+public class PruebaDebugger {
+	
+	public static void main(String[] args) {
+    	ClaseParaDepurar clase = new ClaseParaDepurar();
+    	double valor = clase.echaCuentas();
+    	System.out.println("Sale "+valor);
+    }
+
+}
+
